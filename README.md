88b470e2e1a430b118c8018edf3d5f56cec9ade7
xevidos <xevidos@gmail.com>
2019-01-10 15:05:36 +0000

    Updated Readme, Added Materialize resource

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
-#Any ideas for products or other functionalities go here
+# Any ideas for products or other functionalities go here
 
 - "company managed" so only we can sell the products
 - basically e-bay: buying and selling (authenticated) user's products
 
-#Products to Sell:
+# Products to Sell:
 
-- 
+- Possible Books
 
-#Functionalities:
+# Functionalities:
 
 - Materialize's cards to display products
 
-#Frameworks, Libraries, ETC.
-- Node
+# Frameworks, Libraries, ETC.
+- PHP and possibly some Node
 - Materialize
 - Stripe
 - MongoDB
--- index.php
+++ index.php
@@ -2,13 +2,19 @@
 <head>
 	<link rel="stylesheet" type="text/css" href="./assets/css/materialize.css?v=<?php echo date( "U" );?>">
 	<link rel="stylesheet" type="text/css" href="./assets/css/styles.css?v=<?php echo date( "U" );?>">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<script src="./assets/js/m.js?v=<?php echo date( "U" );?>"></script>
 	<script src="./assets/js/v.js?v=<?php echo date( "U" );?>"></script>
 	<script src="./assets/js/c.js?v=<?php echo date( "U" );?>"></script>
 	<script src="./assets/js/materialize.js?v=<?php echo date( "U" );?>"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <html>
 	<body>
+		<div class="container">
+			<h1>Website title here</h1>
+			<h5>A website by the capstone club</h5>
+		</div>
 		<script>
 			/*global c*/
 			c.initialize();
